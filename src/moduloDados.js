d5176027c3220ada77f494ea19ba33ebd1cc0b77
Sidnasciment0 <sid.nas@outlook.com>
2021-12-22 07:18:48 -0300

    Implementação do modal de comunicação.

--- src/moduloDados.js
+++ src/moduloDados.js
@@ -2,6 +2,5 @@ const userLogin = [
     {id: 1001, username: "sidnascimento", senha: "teste", email: "sid.nas@outlook.com"},
     {id: 1002, username: "victoricoma", senha: "gordinho123", email: "victor.icoma@soulcodeacademy.org"}
 ];
-
 export default userLogin;
 
